ac18917a469dd7dcf202fc601de4f5e7104a4f15
Gabriel Eduardo Monsani <gabrieleduardomonsani@hotmail.com>
2024-05-09 22:01:40 -0300

    Adicionei arquivos do portifolio

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Mim - Gabriel Monsani</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+
+<body>
+
+<div class="main-container">
+    <div class="container">
+        <img class="profile-img" src="caminho/para/sua/imagem.jpg" alt="">
+    </div>
+
+<div class="container">
+    <div class="square">
+        <img class="profile-img" src="style/fotogabriel.png" alt="">
+        <h3>OLÁ, EU SOU O GABRIEL</h3>
+</div>
+</div>
+    <div class="container">
+        <h1>Sobre Mim</h1>
+        <p>
+            <b>
+                <h2>
+                Olá, meu nome é Gabriel Eduardo Monsani, tenho 18 anos e estou cursando o terceiro período de Engenharia de Software na Faculdade Uniguaçu - FAESI. Atualmente trabalho como Técnico de T.I na empresa Friella.
+                </h2>
+            </b>
+        </p>
+    </div>
+</div>
+
+</body>
+</html>
